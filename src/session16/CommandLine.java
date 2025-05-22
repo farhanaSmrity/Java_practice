0f6a38af2cc77e4fcdc9bcb6818389a28114f211
Farhana Sultana <farhanaSmrity@gmail.com>
2025-05-22 10:01:36 +0600

    Command Line Arguments

---
+++ src/session16/CommandLine.java
@@ -0,0 +1,14 @@
+package session16;
+
+public class CommandLine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(String name:args)
+ 		{
+		System.out.println("Hello " + name);
+		}
+	}
+
+}
